f0ccd2e5790a98c580964baa1738c2b597817ac0
Vladimir Vinogradov <qwerty-off@mail.ru>
2021-07-29 23:57:45 +0300

    94-pin pinout

---
+++ step2_copy_hellen154hyundai.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd hellen-one
+python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "hellen154hyundai" "a"
+
+echo "Done!"
---
+++ step3_create_hellen154hyundai.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd hellen-one
+sh bin/create_board.sh ".." "hellen154hyundai" "a" "bom_replace_hellen154hyundai-a.csv"
