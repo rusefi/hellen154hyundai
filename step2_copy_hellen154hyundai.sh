61f0c09ef8f68ad79f920ca9733af53e7d676501
Vladimir Vinogradov <qwerty-off@mail.ru>
2021-09-12 02:26:33 +0300

    Preview outputs

--- step2_copy_hellen154hyundai.sh
+++ step2_copy_hellen154hyundai.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "hellen154hyundai" "a"
+python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "154hyundai" "a"
 
 echo "Done!"
--- step3_create_hellen154hyundai.sh
+++ step3_create_hellen154hyundai.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd hellen-one
-sh bin/create_board.sh ".." "hellen154hyundai" "a" "bom_replace_hellen154hyundai-a.csv"
+sh bin/create_board.sh ".." "154hyundai" "a" "bom_replace_hellen154hyundai-a.csv"
