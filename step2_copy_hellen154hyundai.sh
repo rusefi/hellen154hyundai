8d626e9136164c70cf68c030ddf81ef9639fe1d0
rusefillc <sdfsdfqsf2334234234>
2021-09-20 21:47:43 -0400

    Let's fab rev B with MCU144 not MCU176 #27

--- step2_copy_hellen154hyundai.sh
+++ step2_copy_hellen154hyundai.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "154hyundai" "a"
+python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "154hyundai" "b"
 
 echo "Done!"
